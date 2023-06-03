c222b7ea38495ce3291727679f4680e52d1e2421
Sychic <47618543+Sychic@users.noreply.github.com>
2023-06-03 18:12:48 -0400

    feat: add embeds to verify command

--- src/main/java/live/mcparty/warden/discord/commands/impl/VerifyCommand.java
+++ src/main/java/live/mcparty/warden/discord/commands/impl/VerifyCommand.java
@@ -2,10 +2,15 @@ package live.mcparty.warden.discord.commands.impl;
 
 import live.mcparty.warden.Warden;
 import live.mcparty.warden.discord.commands.IDiscordCommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+
+import java.awt.*;
 
 public class VerifyCommand implements IDiscordCommand {
     @Override
@@ -27,10 +32,28 @@ public class VerifyCommand implements IDiscordCommand {
             String code = event.getOption("code").getAsString();
             boolean success = Warden.getInstance().getVerificationHandler().verifyUser(code, event.getUser().getIdLong());
             if (success) {
-                interactionHook.sendMessage("Thank you for verifying! You've now been whitelisted. Please reconnect.").queue();
+                interactionHook.sendMessage(createSuccessEmbed()).queue();
             } else {
-                interactionHook.sendMessage("Your code was either invalid or expired. Please try again.").queue();
+                interactionHook.sendMessage(createFailureEmbed()).queue();
             }
         });
+    }
+
+    private MessageCreateData createSuccessEmbed() {
+        MessageEmbed embed = new EmbedBuilder()
+                .setAuthor("Verified!", null, "https://cdn.discordapp.com/icons/421459800757501952/255e24acfe657af4f0a01067d58ff99d.png")
+                .setColor(Color.CYAN)
+                .setDescription("Thank you for verifying! You've now been whitelisted." + (Warden.isMigratoryPeriod ? "" : "Please reconnect."))
+                .build();
+        return MessageCreateData.fromEmbeds(embed);
+    }
+
+    private MessageCreateData createFailureEmbed() {
+        MessageEmbed embed = new EmbedBuilder()
+                .setAuthor("Error", null, "https://cdn.discordapp.com/icons/421459800757501952/255e24acfe657af4f0a01067d58ff99d.png")
+                .setColor(Color.RED)
+                .setDescription("Your code was either invalid or expired. Please try again.")
+                .build();
+        return MessageCreateData.fromEmbeds(embed);
     }
 }
