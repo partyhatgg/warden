c340737dd67b3ddb931188c36c1056e3ce306285
Sychic <47618543+Sychic@users.noreply.github.com>
2023-06-03 16:22:01 -0400

    chore: fix build not shading libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     java
 }
 
@@ -16,11 +17,31 @@ repositories {
         url = uri("https://oss.sonatype.org/content/groups/public/")
     }
 }
+val shadowMe: Configuration by configurations.creating {
+    configurations.implementation.get().extendsFrom(this)
+}
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
-    implementation("net.dv8tion:JDA:5.0.0-beta.9")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    shadowMe("net.dv8tion:JDA:5.0.0-beta.9")
+    shadowMe("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+}
+
+java {
+    withSourcesJar()
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        configurations = listOf(shadowMe)
+    }
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
 
 //def targetJavaVersion = 17
