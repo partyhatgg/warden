bb5c5cb3bbe0fd3e85a493d0c370271fd0613590
sychic <47618543+Sychic@users.noreply.github.com>
2024-02-26 22:58:05 -0500

    chore: upgrade jda and add adventure api

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,8 +7,9 @@ repositories {
 }
 
 dependencies {
-    api(bundle("net.dv8tion:JDA:5.0.0-beta.9")!!)
+    api(bundle("net.dv8tion:JDA:5.0.0-beta.20")!!)
     api(bundle("com.fasterxml.jackson.core:jackson-databind:2.15.2")!!)
     compileOnly("com.google.code.gson:gson:2.10")
     compileOnly("com.google.guava:guava:31.1-jre")
+    compileOnly("net.kyori:adventure-api:4.14.0")
 }
